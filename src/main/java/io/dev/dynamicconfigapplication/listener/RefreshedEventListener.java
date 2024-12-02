c4f81f6de18d7744e74b4ba5bd5544db622e4258
maadhuhvn <madyvj3@gmail.com>
2024-12-02 16:10:10 +0530

    first working version of spring cloud kubernetes

--- src/main/java/io/dev/dynamicconfigapplication/listener/RefreshedEventListener.java
+++ src/main/java/io/dev/dynamicconfigapplication/listener/RefreshedEventListener.java
@@ -1,24 +1,18 @@
 package io.dev.dynamicconfigapplication.listener;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.cloud.endpoint.event.RefreshEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Slf4j
 @Component
 public class RefreshedEventListener {
 
-    @EventListener(RefreshScopeRefreshedEvent.class)
-    public void onRefresh(RefreshScopeRefreshedEvent event) {
+    @EventListener({RefreshEvent.class})
+    public void onRefresh(RefreshEvent event) {
         log.info("^^^^^^^^^^^^^^^^^^");
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(event.getTimestamp()), ZoneId.systemDefault());
-        log.info("refresh has happened at {}", localDateTime);
+        log.info("Configuration reloaded: {}", event.getSource());
         log.info("^^^^^^^^^^^^^^^^^^");
-
     }
 }
